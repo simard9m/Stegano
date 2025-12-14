dce725d968c2bae2e12dba42ccf7d51efdb89192
Emile Simard <1832183@etu.cchic.ca>
2025-12-14 14:22:16 -0500

    init

--- src/stegano.py
+++ src/stegano.py
@@ -34,7 +34,7 @@ def _set_lsb(value:int,bit:int)->int:
 
 def cacher_secret(chemin_input:Path,chemin_output:Path,secret:str):
     #charger l'image et convertir en rgb et getr les tuiple rgb ainsi que la taille de l<img
-    img = Image.open(chemin_input).convert("RGB")
+    img = Image.open(chemin_input).convert("RGBA")
     largeur,hauteur = img.size
     pixels = list(img.getdata()) 
     
@@ -59,7 +59,7 @@ def cacher_secret(chemin_input:Path,chemin_output:Path,secret:str):
     new_pixels = []
     bit_index = 0
     
-    for (r, g, b) in pixels:
+    for (r, g, b, a) in pixels:
         if bit_index < nb_bits:
             r = _set_lsb(r, bits_a_rentrer[bit_index])
             bit_index += 1
@@ -70,10 +70,10 @@ def cacher_secret(chemin_input:Path,chemin_output:Path,secret:str):
             b = _set_lsb(b, bits_a_rentrer[bit_index])
             bit_index += 1
             
-        new_pixels.append((r, g, b))
+        new_pixels.append((r, g, b, a))
         
     #creer la nouvelle image et sauvegarder
-    new_img = Image.new("RGB", (largeur, hauteur))
+    new_img = Image.new("RGBA", (largeur, hauteur))
     new_img.putdata(new_pixels)
     #chemin_output.parent.mkdir(parents=True, exist_ok=True)
     new_img.save(chemin_output, format="PNG")
@@ -81,14 +81,14 @@ def cacher_secret(chemin_input:Path,chemin_output:Path,secret:str):
     
 def reveler_secret(image_path:Path):
     #meme chose on get l<image la mets en rgb get la size  et les pixels
-    img = Image.open(image_path).convert("RGB")
+    img = Image.open(image_path).convert("RGBA")
     largeur,hauteur = img.size
     pixels = list(img.getdata())
 
     bits = []
     
     #Lire toutes les LSB en ordre RGB
-    for (r, g, b) in pixels:
+    for (r, g, b, a) in pixels:
         bits.append(r & 1)
         bits.append(g & 1)
         bits.append(b & 1)
@@ -112,4 +112,37 @@ def reveler_secret(image_path:Path):
     
     print(f'--> secret message is: "{secret}"')
 
-reveler_secret(r"c:\Users\Émile\Desktop\Stegano\output\sonic22_Setganise.png")
+def build_arg_parser():
+    parser = argparse.ArgumentParser(
+        prog="stegano",
+        description="Simple PNG LSB steganography CLI"
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # Sous-commande hide
+    hide_parser = subparsers.add_parser("hide", help="Cacher un secret dans un PNG")
+    hide_parser.add_argument("-i", "--input", required=True, help="Chemin vers une image png")
+    hide_parser.add_argument("-o", "--output", required=True, help="Chemin vers l'output")
+    hide_parser.add_argument("-s", "--secret", required=True, help="String secrete a cacher")
+
+    # Sous-commande reveal
+    reveal_parser = subparsers.add_parser("reveal", help="Reveler le secret d'une image PNG")
+    reveal_parser.add_argument("image", help="Chemin vers une image qui contient un secret")
+
+    return parser
+
+def main():
+    parser = build_arg_parser()
+    args = parser.parse_args()
+
+    if args.command == "hide":
+        cacher_secret(Path(args.input), Path(args.output), args.secret)
+    elif args.command == "reveal":
+        reveler_secret(Path(args.image))
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
